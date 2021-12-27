25877e99e964c5a185c5156e9949f061e6c4cd09
glenn9828 <glenn9828@gmail.com>
2021-12-27 18:09:00 +0700

    new

--- projects/glexy-ams/src/app/pages/permission-detail/permission-detail.module.ts
+++ projects/glexy-ams/src/app/pages/permission-detail/permission-detail.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PermissionDetailRoutingModule } from './permission-detail-routing.module';
 import { PermissionDetailListComponent } from './permission-detail-list/permission-detail-list.component';
 import { PermissionDetailModifyComponent } from './permission-detail-modify/permission-detail-modify.component';
+import { FormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 
 @NgModule({
@@ -13,7 +17,11 @@ import { PermissionDetailModifyComponent } from './permission-detail-modify/perm
   ],
   imports: [
     CommonModule,
-    PermissionDetailRoutingModule
+    PermissionDetailRoutingModule,
+    FormsModule,
+    TableModule,
+    InputTextModule,
+    ButtonModule
   ]
 })
 export class PermissionDetailModule { }
--- projects/glexy-ams/src/app/pages/roles/roles-list/roles-list.component.ts
+++ projects/glexy-ams/src/app/pages/roles/roles-list/roles-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Roles } from '@models/roles';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-roles-list',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RolesListComponent implements OnInit {
 
+  listRoles: Roles[] = []
+  selectedRoles: Roles[] = []
+
+  private unSubs?: Subscription;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.listRoles = [
+      {code: 'SA', nameRole:'super admin'},
+      {code: 'ADM', nameRole:'admin'},
+      {code: 'HR', nameRole:'human resource'}
+    ]
+
+  }
+
+  ngOnDestroy(): void {
+    this.unSubs?.unsubscribe()
   }
 
 }
+
+
