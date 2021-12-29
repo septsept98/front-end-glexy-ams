c1fcc264e288b3ac8b151f996583a7b96933d436
glenn9828 <glenn9828@gmail.com>
2021-12-29 18:11:46 +0700

    new

--- projects/glexy-ams/src/app/pages/users/users-list/users-list.component.ts
+++ projects/glexy-ams/src/app/pages/users/users-list/users-list.component.ts
@@ -1,5 +1,7 @@
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DeleteResDto } from '@dto/all-respons/delete-res-dto';
 import { Users } from '@models/users';
 import { UsersService } from '@services/users/users.service';
 import { Subscription } from 'rxjs';
@@ -13,15 +15,34 @@ export class UsersListComponent implements OnInit {
 
   listUsers: Users[] = []
   selectedUsers: Users[] = []
-
+  deleteResDto :DeleteResDto = new DeleteResDto()
   private unSubs?: Subscription;
 
-  constructor(private usersService :UsersService) { }
+  constructor(private usersService :UsersService, private router :Router) { }
 
   ngOnInit(): void {
 
     this.usersService.getAll()?.subscribe(result => this.listUsers = result)
 
+  }
+
+  onCreate() :void{
+
+    this.router.navigateByUrl('/glexy/users/new')
+
+  }
+
+  onUpdate(id :string) :void{
+
+    this.router.navigateByUrl(`/glexy/users/${id}`)
+
+  }
+
+  onDelete(id :string) :void{
+
+    this.usersService.delete(id)?.subscribe(result =>{this.deleteResDto = result
+    this.usersService.getAll()?.subscribe(result => this.listUsers = result)
+    } )
   }
 
   ngOnDestroy(): void {
--- projects/glexy-ams/src/app/pages/users/users-modify/users-modify.component.ts
+++ projects/glexy-ams/src/app/pages/users/users-modify/users-modify.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { InsertResDto } from '@dto/all-respons/insert-res-dto';
+import { UpdateResDto } from '@dto/all-respons/update-res-dto';
+import { Company } from '@models/company';
+import { Employee } from '@models/employee';
+import { Roles } from '@models/roles';
+import { Users } from '@models/users';
+import { CompanyService } from '@services/company/company.service';
+import { RolesService } from '@services/roles/roles.service';
+import { UsersService } from '@services/users/users.service';
 
 @Component({
   selector: 'app-users-modify',
@@ -7,9 +17,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersModifyComponent implements OnInit {
 
-  constructor() { }
+  data:Users = new Users()
+  role:Roles = new Roles()
+  employee:Employee = new Employee()
+  company:Company = new Company
+  insertResDto :InsertResDto = new InsertResDto()
+  updateResDto :UpdateResDto = new UpdateResDto()
+  UserId? :string|null
+  listCompany :Company[] = []
+  listRoles :Roles[] = []
+  save :boolean = true
+  selectedFile!:FileList
+  file! :File |null 
+
+  constructor(private companyService:CompanyService, private rolesService :RolesService,
+    private usersService:UsersService, private activatedRoute :ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.UserId = this.activatedRoute.snapshot.paramMap.get('id')
+
+    if(this.UserId){
+
+      this.usersService.getById(this.UserId)?.subscribe(result=>{
+
+        this.save = false
+         this.data = result
+      })
+      
+    }
+
+    this.data.employeeId = this.employee
+    this.data.rolesId = this.role
+    this.data.employeeId.companyId = this.company
+    
+    this.companyService.getAll()?.subscribe(result => this.listCompany = result)
+    this.rolesService.getAll()?.subscribe(result => this.listRoles = result)
+
+  }
+
+  selectFile(event :any){
+
+    this.selectedFile = event.target.files
+    
+  }
+
+  add(){
+    this.data.employeeId.emailEmployee = this.data.email
+    if(this.UserId){
+      
+      this.usersService.update(this.data)?.subscribe(result => this.updateResDto = result)
+    }else{
+    this.file = this.selectedFile?.item(0)
+    this.usersService.insert(this.data,this.file)?.subscribe(result =>{
+      this.insertResDto = result
+    })
+  }
   }
 
 }
