85ab6c5b6057398559a7d21eda93bc3bacb56c45
septsept98 <septianardi053@gmail.com>
2021-12-31 14:13:58 +0700

    new sept

--- projects/glexy-ams/src/app/pages/component/general-template.component.ts
+++ projects/glexy-ams/src/app/pages/component/general-template.component.ts
@@ -18,50 +18,35 @@ export class GeneralTemplateComponent implements OnInit {
   }
 
   iconOnly(): void {
-    const parentDiv = document.getElementById('parent-body')
-    const btnSideBar = document.getElementById('btn-sidebar')
-    if (btnSideBar?.className.match('is-active')) {
-      btnSideBar?.classList.remove('is-active')
+    const body = document.getElementsByTagName('body')[0]
+    if (body?.className.match('sidebar-icon-only')) {
+      body?.classList.remove('sidebar-icon-only')
     } else {
-      btnSideBar?.classList.add('is-active')
-    }
-    if (parentDiv?.className.match('closed-sidebar')) {
-      parentDiv?.classList.remove('closed-sidebar')
-    } else {
-      parentDiv?.classList.add('closed-sidebar')
-    }
-  }
-
-  closeSidebar(): void {
-    const parentDiv = document.getElementById('parent-body')
-    const btnSideBar = document.getElementById('btn-sidebar-mobile')
-    if (btnSideBar?.className.match('is-active')) {
-      btnSideBar?.classList.remove('is-active')
-    } else {
-      btnSideBar?.classList.add('is-active')
-    }
-    if (parentDiv?.className.match('sidebar-mobile-open')) {
-      parentDiv?.classList.remove('sidebar-mobile-open')
-    } else {
-      parentDiv?.classList.add('sidebar-mobile-open')
+      body?.classList.add('sidebar-icon-only')
     }
   }
 
   openMenu(): void {
-    const menu = document.getElementById('menu-mobile')
-    const btnMenuMob = document.getElementById('btn-menu-mobile')
-    if (btnMenuMob?.className.match('active')) {
-      btnMenuMob?.classList.remove('active')
+    const menu = document.getElementById('sidebar')
+    if (menu?.className.match('active')) {
+      menu?.classList.remove('active')
     } else {
-      btnMenuMob?.classList.add('active')
-    }
-    if (menu?.className.match('header-mobile-open')) {
-      menu?.classList.remove('header-mobile-open')
-    } else {
-      menu?.classList.add('header-mobile-open')
+      menu?.classList.add('active')
     }
   }
 
- 
+  hoverOpen(data: any): void {
+    const body = document.getElementsByTagName('body')[0]
+    if (body?.className.match('sidebar-icon-only')) {
+      data.classList.add('hover-open')
+    }
+  }
+
+  hoverClose(data: any): void {
+    const body = document.getElementsByTagName('body')[0]
+    if (body?.className.match('sidebar-icon-only')) {
+      data.classList.remove('hover-open')
+    }
+  }
 
 }
