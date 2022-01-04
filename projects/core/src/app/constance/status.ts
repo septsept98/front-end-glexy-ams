aa19bf8b893feebd27ed6ab84df807c8a4912a29
febri228 <febri.ufairahasan20@gmail.com>
2022-01-04 18:34:23 +0700

    new febri

---
+++ projects/core/src/app/constance/status.ts
@@ -0,0 +1,6 @@
+export const statusAss = new Map<number, string>()
+statusAss.set(1, 'Deployable')
+statusAss.set(2, 'Undeployble')
+statusAss.set(3, 'Archived')
+statusAss.set(4, 'Pending')
+statusAss.set(5, 'Assign')
--- projects/glexy-ams/src/app/pages/location/location-list/location-list.component.ts
+++ projects/glexy-ams/src/app/pages/location/location-list/location-list.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Location } from '@models/location';
 import { LocationService } from '@services/location/location.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-location-list',
   templateUrl: './location-list.component.html',
   styleUrls: ['./location-list.component.css']
 })
-export class LocationListComponent implements OnInit {
+export class LocationListComponent implements OnInit, OnDestroy {
 
-  constructor(private locationService : LocationService) { }
+  constructor(private locationService : LocationService, private router : Router) { }
   selectedLocation: Location[] = []
   dataList: Location[] = []
   obs? : Subscription
 
   ngOnInit(): void {
     this.locationService.getAll()?.subscribe(result => this.dataList = result)
+  }
+
+  onUpdate(id : number): void{
+    this.router.navigateByUrl(`/glexy/location/${id}`)
+  }
+
+  ngOnDestroy(): void {
+    this.obs?.unsubscribe
   }
 
 }
